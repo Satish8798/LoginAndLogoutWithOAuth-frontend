ffc8a5a12926e9bfe65bd95ddb91154ff4c8fb7a
Satish Ganesan <ganesansatishm7@gmail.com>
2023-01-15 15:28:58 +0530

    add new route for redirected route with access token

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,41 +1,55 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-function Home({user,setUser,loginStatus,setLoginStatus}) {
-    const navigateTo=useNavigate();
+function Home({ user, setUser, loginStatus, setLoginStatus }) {
+  const navigateTo = useNavigate();
   return (
     <div>
-        {
-            loginStatus? 
-            (
-                <div>
-                    <h1>Welcome<span style={{
-                        color: "red"
-                    }}> {user.firstName} {user.lastName} </span>to Login-Logout OAuth App</h1>
-        <h2>Your email id is <span style={{
-            color:"white"
-        }}>{user.email}</span></h2>
-        <img src={user.picture} alt="profilepic" style={{
-            borderRadius:"50%"
-        }}/>
-        <br/>
-        <button type='button' className='btn btn-danger mt-5' onClick={()=>{
-            setLoginStatus(false);
-            setUser({})
-        }}>Log out</button>
-                </div>
-            ):(
-                <div>
-                    <h1>you are not logged in yet</h1>
-                    <h1>please login to enter into Satish's App</h1>
-                    <button className="btn btn-success mt-5" onClick={()=>{
-                        navigateTo('/auth/login');
-                    }}>Log in</button>
-                </div>
-            )
-        }
+      {loginStatus ? (
+        <div className="card" style={{ width: "20rem", backgroundColor: "black", boxShadow:"2px 2px 2px 2px yellow" }}>
+          <img src={user.picture} class="card-img-top m-5" alt="..." style={{
+            borderRadius:"50%",
+            width: 200
+          }}></img>
+          <div class="card-body">
+            <h5 class="card-title">
+              {user.firstName} {user.lastName}
+            </h5>
+            <p class="card-text">
+             {user.email}
+            </p>
+            <p class="card-text">
+              Some users might have kept their email private , so we cannot acceess them in oauth,
+              naming in github is different(last name is not provied)
+            </p>
+            <button
+              type="button"
+              className="btn btn-danger mt-2"
+              onClick={() => {
+                setLoginStatus(false);
+                setUser({});
+              }}
+            >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>you are not logged in yet</h1>
+          <h1>please login to enter into Satish's App</h1>
+          <button
+            className="btn btn-success mt-5"
+            onClick={() => {
+              navigateTo("/auth/login");
+            }}
+          >
+            Log in
+          </button>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
