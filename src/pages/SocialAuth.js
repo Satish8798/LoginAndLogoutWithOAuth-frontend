44c9a5c2e81167787083c1fce735221aca469590
Satish Ganesan <ganesansatishm7@gmail.com>
2023-01-15 21:53:32 +0530

    add facebook login

--- src/pages/SocialAuth.js
+++ src/pages/SocialAuth.js
@@ -60,7 +60,7 @@ function SocialAuth({
           onClick={googleLogin}
         ></i>
 
-       {/*  <a
+        <a
           href={`https://www.facebook.com/v15.0/dialog/oauth?
           client_id=${process.env.REACT_APP_FACEBOOK_APP_ID}
           &redirect_uri=${encodeURIComponent(
@@ -69,7 +69,7 @@ function SocialAuth({
         >
           <i className="bi bi-facebook ms-3 fs-2 text-primary"></i>
         </a>
- */}
+
         <a
           href={`https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}`}
         >
